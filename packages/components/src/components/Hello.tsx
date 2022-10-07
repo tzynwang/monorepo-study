185d73d0143416eecb8ebaeb9861873021641280
Charlie <charlie@lawsnote.com>
2022-10-07 17:41:01 +0800

    [feat] use .module.css for Hello

--- packages/components/src/components/Hello.tsx
+++ packages/components/src/components/Hello.tsx
@@ -1,8 +1,10 @@
 import React, { memo } from 'react';
+import cn from 'classnames';
+import scopedStyle from './index.module.css';
 
 function Hello(): React.ReactElement {
   /* Main */
-  return <div>hello world 😂</div>;
+  return <div className={cn(scopedStyle.scopedHelloStyle)}>hello world 😂</div>;
 }
 
 export default memo(Hello);
